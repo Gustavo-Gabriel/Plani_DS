eac45b9fdfbf072a4290a0b07fc8cab05fb1bfc4
Gustavo Anjos <gustavo.anjos@popcode.com.br>
2021-10-19 08:56:19 -0400

    add radius

---
+++ Sources/Plani_DS/Tokens/Radius.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+public enum Radius {
+
+    /// Returns value = 5
+    public static let small: CGFloat = 5
+
+    /// Returns value = 10
+    public static let medium: CGFloat = 10
+
+    /// Returns value = 20
+    public static let large: CGFloat = 20
+}
