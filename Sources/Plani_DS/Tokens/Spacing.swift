95891f09f57c8ada06251e1784e177989a69deec
Gustavo Anjos <gustavo.anjos@popcode.com.br>
2021-10-19 08:38:12 -0400

    add Spacing

--- Sources/Plani_DS/Tokens/Spacing.swift
+++ Sources/Plani_DS/Tokens/Spacing.swift
@@ -7,16 +7,16 @@ public enum Spacing {
 
     /// Returns value = 10
     public static let small: CGFloat = 10
-    
+
     /// Returns value = 20
     public static let medium: CGFloat = 20
-    
+
     /// Returns value = 25
     public static let large: CGFloat = 25
-    
+
     /// Returns value = 30
     public static let xLarge: CGFloat = 30
-    
+
     /// Returns value = 40
     public static let xxLarge: CGFloat = 40
 }
--- Tests/Plani_DSTests/Tokens/SpacingTests.swift
+++ Tests/Plani_DSTests/Tokens/SpacingTests.swift
@@ -1,50 +1,14 @@
-import Quick
-import Nimble
+import XCTest
 
 @testable import Plani_DS
 
-final class SpacingTests: QuickSpec{
-    override func spec() {
-        describe("xSmall") {
-            it("return 5") {
-                expect(Spacing.xSmall).to(equal(5))
-            }
-        }
-
-        describe("small") {
-            it("return 10") {
-                expect(Spacing.small).to(equal(10))
-            }
-        }
-
-        describe("medium") {
-            it("return 20") {
-                expect(Spacing.medium).to(equal(20))
-            }
-        }
-        
-        describe("medium") {
-            it("return 20") {
-                expect(Spacing.medium).to(equal(20))
-            }
-        }
-        
-        describe("large") {
-            it("return 25") {
-                expect(Spacing.large).to(equal(25))
-            }
-        }
-        
-        describe("xLarge") {
-            it("return 30") {
-                expect(Spacing.xLarge).to(equal(30))
-            }
-        }
-        
-        describe("xxLarge") {
-            it("return 40") {
-                expect(Spacing.xxLarge).to(equal(40))
-            }
-        }
+final class SpacingTests: XCTestCase {
+    func testValuesOfSpacings() {
+        XCTAssertEqual(Spacing.xSmall, 5)
+        XCTAssertEqual(Spacing.small, 10)
+        XCTAssertEqual(Spacing.medium, 20)
+        XCTAssertEqual(Spacing.large, 25)
+        XCTAssertEqual(Spacing.xLarge, 30)
+        XCTAssertEqual(Spacing.xxLarge, 40)
     }
 }
