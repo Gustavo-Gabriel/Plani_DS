5b3c36902fc0991a5ebd4c7b66d9aebefae6e606
Vinícius de Oliveira Ribeiro <vcribeiro88@gmail.com>
2021-10-26 12:03:29 -0400

    [FDB-7] Fix: Colors asset export fix

---
+++ Sources/External/External.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+public enum sharedResources {
+    static public let bundle: Bundle = Bundle.module
+}
--- Sources/Plani_DS/Assets/PlaniAssets.swift
+++ Sources/Plani_DS/Assets/PlaniAssets.swift
@@ -1,14 +1,15 @@
 import SwiftUI
+import Foundation
 
 @available(iOS 14, *)
 final class PlaniAssets {
     static func colorFromName(_ name: String, opacity: Double = 1.0) -> Color? {
-        return Color(name, bundle: Bundle.module).opacity(opacity)
+        return Color(name, bundle: .module).opacity(opacity)
     }
     
     static func imageFromName(_ name: String) -> UIImage? {
         return UIImage(named: name,
-                       in: Bundle.module,
+                       in: .module,
                        compatibleWith: nil)
     }
 }
