d1cf7b42ca01705747f2ca160695385a58f230d1
Vinícius de Oliveira Ribeiro <vcribeiro88@gmail.com>
2021-11-01 10:57:18 -0400

    [FDB-16] Refactor: public class

--- Sources/Plani_DS/Components/Card/AddCard.swift
+++ Sources/Plani_DS/Components/Card/AddCard.swift
@@ -1,8 +1,29 @@
-//
-//  File.swift
-//  
-//
-//  Created by Vinícius de Oliveira on 01/11/21.
-//
+import SwiftUI
 
-import Foundation
+@available(iOS 14, *)
+public struct AddCard: View {
+    var title: String
+    var icon: String
+    
+    init(title: String, icon: String) {
+        self.title = title
+        self.icon = icon
+    }
+    
+    public var body: some View {
+        VStack {
+            Image(systemName: icon)
+            Text("Adicionar")
+        }
+        .foregroundColor(Colors.orange100)
+        .shadow(color: .black.opacity(0.1), radius: 5, x: 0, y: 4)
+        .cornerRadius(20)
+    }
+}
+
+@available(iOS 14, *)
+struct AddCard_Preview: PreviewProvider {
+    static var previews: some View {
+        AddCard(title: "Adicionar", icon: "square.and.arrow.up")
+    }
+}
--- Sources/Plani_DS/Components/Card/PlaniCard.swift
+++ Sources/Plani_DS/Components/Card/PlaniCard.swift
@@ -1,23 +1,19 @@
-import SwiftUI
-
-@available(iOS 13, *)
-public class PlaniCard {
-    private var card: PlaniCardType
-    
-    init(card: PlaniCardType) {
-        self.card = card
-    }
-}
-
-@available(iOS 14, *)
-extension PlaniCard {
-    public func createAddCard(title: String, icon: String) -> some View {
-        VStack {
-            Image(systemName: icon)
-            Text("Adicionar")
-        }
-        .foregroundColor(Colors.orange100)
-        .shadow(color: .black.opacity(0.1), radius: 5, x: 0, y: 4)
-        .cornerRadius(20)
-    }
-}
+//import SwiftUI
+//
+//@available(iOS 14, *)
+//public class PlaniCard {
+//    private var card: PlaniCardType
+//}
+//
+//@available(iOS 14, *)
+//extension PlaniCard {
+//    private func AddCard(title: String, icon: String) -> some View {
+//        VStack {
+//            Image(systemName: icon)
+//            Text("Adicionar")
+//        }
+//        .foregroundColor(Colors.orange100)
+//        .shadow(color: .black.opacity(0.1), radius: 5, x: 0, y: 4)
+//        .cornerRadius(20)
+//    }
+//}
