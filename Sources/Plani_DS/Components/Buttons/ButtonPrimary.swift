11f27ec1c2276585c11300d35e158a6a06e160f5
Gustavo Anjos <gustavo.anjos@popcode.com.br>
2021-11-02 20:40:21 -0400

    add Action in ButtonPrimary

--- Sources/Plani_DS/Components/Buttons/ButtonPrimary.swift
+++ Sources/Plani_DS/Components/Buttons/ButtonPrimary.swift
@@ -4,30 +4,34 @@ import SwiftUI
 public struct ButtonPrimary: View {
     var title: String
     var icon: String
+    var action: () -> Void
 
-    public init(title: String, icon: String = "") {
+    public init(title: String, icon: String = "", action: @escaping () -> Void = {}) {
         self.title = title
         self.icon = icon
+        self.action = action
     }
 
     public var body: some View {
         GeometryReader { geometry in
-            VStack {
-                HStack {
-                    Image(icon)
-                        .foregroundColor(Colors.orange300)
-                        .padding(.bottom, Spacing.medium)
-            
-                    Text(title)
-                        .font(.system(size: 18))
-                        .foregroundColor(.white)
+            Button(action: action, label: {
+                VStack {
+                    HStack {
+                        Image(icon)
+                            .foregroundColor(Colors.orange300)
+                            .padding(.bottom, Spacing.medium)
+                
+                        Text(title)
+                            .font(.system(size: 18))
+                            .foregroundColor(.white)
+                    }
+                    .padding(.horizontal, Spacing.medium)
+                    .padding(.vertical, 8)
                 }
-                .padding(.horizontal, Spacing.medium)
-                .padding(.vertical, 8)
-            }
-            .frame(minWidth: geometry.size.width, idealHeight: 40, alignment: .center)
-            .background(Colors.purple300)
-            .cornerRadius(Radius.large)
+                .frame(minWidth: geometry.size.width, idealHeight: 40, alignment: .center)
+                .background(Colors.purple300)
+                .cornerRadius(Radius.large)
+            })
         }
     }
 }
